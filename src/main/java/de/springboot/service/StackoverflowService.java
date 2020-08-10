8986dca9a19eb73de39de7cc33db9554244f3b57
alexeideriglazov <alexeideriglazov@gmail.com>
2020-08-10 20:50:42 +0300

    Getting info from mongoDB

--- src/main/java/de/springboot/service/StackoverflowService.java
+++ src/main/java/de/springboot/service/StackoverflowService.java
@@ -1,22 +1,35 @@
 package de.springboot.service;
 
 import de.springboot.model.StackoverflowWebsite;
+import de.springboot.persistance.StackoverflowWebsiteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class StackoverflowService {
-    private static List<StackoverflowWebsite> items = new ArrayList<>();
-    static {
-        items.add(new StackoverflowWebsite("stackoverflow","http://stackoverflow.com","http://cdn.sstatic.net/Sites/stackoverflow/img/favicon.ico", "Stack Overflow","For programmers"));
-        items.add(new StackoverflowWebsite("stackoverflowru","http://ru.stackoverflow.com","http://cdn.sstatic.net/Sites/ru/img/favicon.ico", "Stack Overflow Russia","Для программистов"));
+    @Autowired
+    private StackoverflowWebsiteRepository repository;
 
-        
+//    private static List<StackoverflowWebsite> items = new ArrayList<>();
+//    static {
+//        items.add(new StackoverflowWebsite("stackoverflow","http://stackoverflow.com","http://cdn.sstatic.net/Sites/stackoverflow/img/favicon.ico", "Stack Overflow","For programmers"));
+//        items.add(new StackoverflowWebsite("stackoverflowru","http://ru.stackoverflow.com","http://cdn.sstatic.net/Sites/ru/img/favicon.ico", "Stack Overflow Russia","Для программистов"));
+//
+//
+//    }
+/*
+** should start with this method just ones to write something to db
+    @PostConstruct
+    public void init() {
+        repository.saveAll(items);
     }
 
+ */
     public List<StackoverflowWebsite> findAll(){
-        return items;
+        return repository.findAll();
     }
 }
