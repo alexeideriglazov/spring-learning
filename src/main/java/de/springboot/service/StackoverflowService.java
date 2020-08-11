feb79e3e56a56ec84880248efbd0ddabb1de7c0b
alexeideriglazov <alexeideriglazov@gmail.com>
2020-08-11 18:33:22 +0300

    RestTemplate and Stackoverflow API usage

--- src/main/java/de/springboot/service/StackoverflowService.java
+++ src/main/java/de/springboot/service/StackoverflowService.java
@@ -1,18 +1,24 @@
 package de.springboot.service;
 
+import com.google.common.collect.ImmutableList;
+import de.springboot.model.SiteDto;
 import de.springboot.model.StackoverflowWebsite;
 import de.springboot.persistance.StackoverflowWebsiteRepository;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class StackoverflowService {
     @Autowired
     private StackoverflowWebsiteRepository repository;
+    @Autowired
+    private StackExchangeClient stackExchangeClient;
 
 //    private static List<StackoverflowWebsite> items = new ArrayList<>();
 //    static {
@@ -29,7 +35,28 @@ public class StackoverflowService {
     }
 
  */
-    public List<StackoverflowWebsite> findAll(){
-        return repository.findAll();
+
+    public List<StackoverflowWebsite> findAll() {
+        return stackExchangeClient.getSites().stream()
+                .map(this::toStackoverflowWebsite)
+                .filter(this::ignoreMeta)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), ImmutableList::copyOf));
     }
+
+    private boolean ignoreMeta(@NonNull StackoverflowWebsite stackoverflowWebsite) {
+        return !stackoverflowWebsite.getId().startsWith("meta");
+    }
+
+    private StackoverflowWebsite toStackoverflowWebsite(@NonNull SiteDto input) {
+        return new StackoverflowWebsite(
+                input.getSite_url().substring(8, input.getSite_url().length() - 4),
+                input.getSite_url(),
+                input.getFavicon_url(),
+                input.getName(),
+                input.getAudience());
+    }
+
+    //    public List<StackoverflowWebsite> findAll(){
+//        return repository.findAll();
+//    }
 }
