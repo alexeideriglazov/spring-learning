894603ae9b139744060b51783d4019fc46bbe731
alexeideriglazov <alexeideriglazov@gmail.com>
2020-08-11 13:24:47 +0300

    Adding lombock

--- src/main/java/de/springboot/model/StackoverflowWebsite.java
+++ src/main/java/de/springboot/model/StackoverflowWebsite.java
@@ -1,44 +1,22 @@
 package de.springboot.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Document
 public class StackoverflowWebsite {
     @Id
-    private final String id;
+    private  String id;
     //@Field spring will do it automatic
-    private final String website;
-    private final String iconImageUrl;
-    private final String title;
-    private final String description;
-
-    public StackoverflowWebsite(String id, String website, String iconImageUrl, String title, String description) {
-        this.id = id;
-        this.website = website;
-        this.iconImageUrl = iconImageUrl;
-        this.title = title;
-        this.description = description;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public String getIconImageUrl() {
-        return iconImageUrl;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
+    private String website;
+    private String iconImageUrl;
+    private String title;
+    private String description;
 }
